7aa165ffb2b71cfb0c38079981a73744d4dd1daf
nobody <nobody@eaa82560-6425-48e2-a517-1dca51b31b6c>
2000-11-25 11:57:22 +0000

    This commit was manufactured by cvs2svn to create branch 'avendor'.

---
+++ branches/avendor/SOUND/PLAYER.C
@@ -0,0 +1,197 @@
+#include"allegro.h"
+#include"seal.h"
+#include"app.h"
+#include"button.h"
+#include"dialogs.h"
+#include"iodlg.h"
+#include"dlxsound.h"
+
+
+p_play pp = 0;
+l_int file_is_playing = 0;  // 0 = false
+
+p_list get_player_filelist ( void ) {
+
+                                                /* function for free-ing items */
+  p_list p = list_init(malloc(sizeof(t_list)), &free_filehistory_item, 0);
+
+  if ( p ) {
+
+     p->insert(p, new_filehistory_item("MPEG Layer 2/3 (*.mp?)", "*.mp?"));
+     p->insert(p, new_filehistory_item("MIDI (*.mid)", "*.mid"));
+     p->insert(p, new_filehistory_item("MOV (*.mov)", "*.mov"));
+//     p->insert(p, new_filehistory_item("(*.)", "*."));
+     p->insert(p, new_filehistory_item("All files (*.*)", "*.*"));
+
+  };
+
+
+  return p;
+
+};
+
+
+
+void play(l_text file) {
+
+
+//     seal_error(ERR_INFO, "Want to play file [%s]", file);
+
+     
+     if (!pp->init_file(pp, file))
+     {
+        seal_error(ERR_INFO, "Error opening file [%s]", file);
+        
+     }
+     else
+     {
+     if (!pp->play_file(pp)) return;
+     file_is_playing = 1;
+     }
+
+}
+
+void  poll_func_callback ( p_object s ) /* it's call each second */
+{
+
+     file_is_playing = pp->poll_file(pp);
+     
+};
+
+
+void  player_translate_event ( p_object o, p_event e )
+{
+
+   RETVIEW(o, e);
+//   file_is_playing = pp->poll_file(pp);
+
+   if ( o->phase == PH_PREPROCESS ) {
+
+      if (file_is_playing == 0)
+      {
+         set_event(e, EV_MESSAGE, MSG_CLOSE, o);
+      }
+
+
+
+      if ( e->type == EV_MESSAGE ) {
+
+         switch ( e->message ) {
+         case MSG_CLOSE:
+         pp->stop_file(pp);
+
+         }
+      };
+
+   };
+
+};
+
+
+static t_point   new_win_size_minimum ( p_view o )
+{
+
+  return point_assign(100, 20);
+
+};
+
+void  stat1_func_callback ( p_object s ) /* it's call each second */
+{
+
+
+     TEXTLINE(s)->set_text(TEXTLINE(s),
+                               set_format_text(NULL, "%02d:%02d",
+                               pp->get_time(pp)/60,pp->get_time(pp)%60
+                               ));
+
+};
+
+
+
+
+
+app_begin ( void ) {
+
+  if ( ap_process == AP_INIT ) {
+
+    t_rect r;
+    p_object o = 0;
+    p_object stat = 0;
+    p_textline stat1 = 0;
+    char file[260];
+
+    AP_SETNUMOFCALLS(1);    
+
+    pp = SFA_init();
+
+
+
+    if ( !ap_args ) ap_args = open_dialog("c:/", NULL, get_player_filelist());
+
+    if ( !ap_args ) return;
+
+    strcpy(file, ap_args);
+
+     play(file);
+
+     r = rect_assign(0, 0, 300, 100);
+
+
+     o = appwin_init(malloc(sizeof(t_appwin)), /* make window */
+                            r,
+                            "sfa player .05 beta",
+//                            ap_args,
+                            0x0,
+                            ap_id, /* application id */
+                            &player_translate_event);
+
+     VIEW(o)->size_minimum = &new_win_size_minimum;
+
+     
+     OBJECT(desktop)->insert(OBJECT(desktop), OBJECT(o));
+
+     r = rect_assign(10, 30, 290, 70);
+
+     stat = stattext_init ( malloc(sizeof(t_stattext)),
+					 r, 
+					 TX_ALIGN_LEFT, // limit
+					 "Playing %s ...\n%s",
+                (l_text) ap_args,
+                pp->get_additional_info(pp));
+
+
+     OBJECT(o)->insert(OBJECT(o), OBJECT(stat));
+
+
+//     OBJECT(desktop)->process_tick = 200; /* each second rewrite it */
+     OBJECT(desktop)->func_callback = &poll_func_callback;
+     init_stillprocess ( OBJECT(desktop), 20 );
+
+     
+      r = rect_assign(10, 70, 190, 90);
+
+     stat1 = textline_init( _malloc(sizeof(t_textline)),
+                            r,
+                            255,
+                            TF_REWRITEUNABLE);
+
+     OBJECT(stat1)->func_callback = &stat1_func_callback;
+
+
+     OBJECT(o)->insert(OBJECT(o), OBJECT(stat1));
+
+     stat1->set_text(stat1, "Wellcome!");
+
+     init_stillprocess ( OBJECT(stat1), 1000 );
+
+
+
+  } else
+
+  if ( ap_process == AP_FREE ) {
+
+
+
+  };
+
+} app_end;
