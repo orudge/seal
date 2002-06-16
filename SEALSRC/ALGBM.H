fe6536a0c36eb76d86e9984eb4067f994413370b
Owen Rudge <7374160+orudge@users.noreply.github.com>
2002-06-16 15:01:44 +0000

    This commit was manufactured by cvs2svn to create tag 'seal3-00-test'.

--- SEALSRC/ALGBM.H
+++
@@ -1,2 +0,0 @@
-BITMAP *load_gbm(char *filename, RGB *pal);
-int save_gbm(char *filename, BITMAP *bmp, RGB *pal);
--- SEALSRC/makeaut.bat
+++
@@ -1,8 +0,0 @@
-gcc -g -c autoset.c -o autoset.o
-dlxgen.exe d:/seal/autoset.dlx autoset.o
-
-gcc -g -c astart.c -o astart.o
-dlxgen.exe d:/seal/astart.exe astart.o
-
-
-
